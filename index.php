1651fab868b6e99f6def869cfce19ccd45c107de
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2017-02-08 12:59:36 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+
+$tail = '';
+$lastDayOfPrevMonth = new DateTime('last day of previous month');
+while($lastDayOfPrevMonth->format('w') < '6'){
+	$tail = sprintf('<td class="gray">%d</td>',
+							$lastDayOfPrevMonth->format('d')) . $tail;
+	$lastDayOfPrevMonth->sub(new DateInterval('P1D'));
+}
+
+$body = '';
+$peroid = new DatePeriod(
+	new DateTime('first day of this month'),
+	new DateInterval('P1D'),
+	new DateTime('first day of next month')
+);
+foreach( $peroid as $day) {
+	if($day->format('w') === '0') { $body .= '</tr><tr>'; }
+	$body .= sprintf('<td class="youbi_%d">%d</td>', 
+											$day->format('w'), $day->format('d'));
+}
+
+$head = '';
+$firstDayOfNextMonth = new DateTime('first day of next month');
+while($firstDayOfNextMonth->format('w') > 0){
+	$head .= sprintf('<td class="gray">%d</td>',
+							$firstDayOfNextMonth->format('d')); 
+	$firstDayOfNextMonth->add(new DateInterval('P1D'));
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>Calendar</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+	<table>
+		<thead>
+			<th><a href="">&laquo;</a></th>
+			<th colspan="5">February 2017</th>
+			<th><a href="">&raquo;</a></th>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Sun</td>
+				<td>Mon</td>
+				<td>Tue</td>
+				<td>Wed</td>
+				<td>Thu</td>
+				<td>Fri</td>
+				<td>Sat</td>
+			</tr>
+			<tr>
+				<?php echo $tail . $body . $head; ?>
+<!--
+				<td class="youbi_0">1</td>
+				<td class="youbi_1">2</td>
+				<td class="youbi_2">3</td>
+				<td class="youbi_3 today">4</td>
+				<td class="youbi_4">5</td>
+				<td class="youbi_5">6</td>
+				<td class="youbi_6">7</td>
+			</tr>
+			<tr>
+				<td class="youbi_0">30</td>
+				<td class="youbi_1">31</td>
+				<td class="gray">1</td>
+				<td class="gray">2</td>
+				<td class="gray">3</td>
+				<td class="gray">4</td>
+				<td class="gray">5</td>
+-->
+			</tr>
+		</tbody>
+		<tfoot>
+			<th colspan="7"><a href="">Today</a></th>
+		</tfoot>
+	</table>
+	
+</body>
+</html>
